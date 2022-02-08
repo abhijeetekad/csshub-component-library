10b81a09dafcd03aaf6fb071225f7cc1c008109e
abhijeetekad <ekadabhijeet4@gmail.com>
2022-02-09 02:01:36 +0530

    style: pr review changes

---
+++ documentation/doc.js
@@ -0,0 +1,8 @@
+// const sideBarBtn = document.getElementById("sidebar-btn");
+// const sideBar = document.getElementById("sidebar");
+// // console.log(sideBarBtn);
+// // console.log(sideBar);
+// sideBarBtn.addEventListener("click", () => {
+//   console.log("click");
+//   sidebar.style.left:-210px;
+// });
